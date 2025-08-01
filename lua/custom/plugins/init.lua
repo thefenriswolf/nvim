15cb4ddeacd67714b7b8514bd9c7d5118f701bc7
thefenriswolf <stefan.rohrbacher97@gmail.com>
2025-08-01 16:18:01 +0200

    add plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,49 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'NeogitOrg/neogit',
+    lazy = false,
+    enabled = true,
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+      -- Only one of these is needed.
+      'nvim-telescope/telescope.nvim', -- optional
+      'ibhagwan/fzf-lua', -- optional
+      'echasnovski/mini.pick', -- optional
+    },
+    config = true,
+  },
+  {
+    'catppuccin/nvim',
+    lazy = false,
+    enabled = true,
+    name = 'catppuccin',
+    priority = 1000,
+  },
+  {
+    'epwalsh/obsidian.nvim',
+    enabled = false,
+    lazy = true,
+    version = '*',
+    ft = { 'markdown' },
+    config = true,
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      workspaces = {
+        { name = 'notes', path = '~/notes' },
+        -- {name = "daily", path = "~/org/notes/daily"}
+      },
+      ui = { enable = false },
+    },
+  },
+  {
+    'catppuccin/nvim',
+    lazy = false,
+    enabled = true,
+    name = 'catppuccin',
+    priority = 1000,
+  },
+}
