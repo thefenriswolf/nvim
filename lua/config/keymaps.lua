e275d18e162602d256d8d27b3a51c8f9fd306537
thefenriswolf <stefan.rohrbacher97@gmail.com>
2025-10-22 10:55:41 +0200

    splitkeys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,13 +2,18 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- switch to other open window
+-- CTRL-w w
+-- or
+-- CTRL-w CTRL-w
+
 -- Clear highlights on search when pressing <Esc> in normal mode
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- vs - Makes vertical split
 vim.keymap.set("n", "vs", "<C-W>v", { desc = "Vertical Split" })
 -- ss - Makes horizontal split
-vim.keymap.set("n", "ss", "<C-W>s", { desc = "Horizontal Split" })
+vim.keymap.set("n", "hs", "<C-W>s", { desc = "Horizontal Split" })
 
 -- Diagnostic keymaps
 vim.keymap.set("n", "<leader>q", function()
